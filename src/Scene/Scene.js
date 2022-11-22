092529e904d3a49659d2e8c4b79b713931a082ac
Laura Hiller <Hillerelaura@gmail.com>
2022-11-22 12:16:28 -0800

    Added videos, wrote shader for lamp, removed bloom pass

--- src/Scene/Scene.js
+++ src/Scene/Scene.js
@@ -1,32 +1,40 @@
-import {React, Suspense} from 'react';
+import {React, Suspense, useState} from 'react';
 import './Scene.css';
-import { Canvas, extend  } from '@react-three/fiber';
-import { Effects, OrbitControls, Stats } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Stats, PerspectiveCamera, Preload, BakeShadows } from '@react-three/drei';
 import * as THREE from 'three';
-import { UnrealBloomPass } from 'three-stdlib';
-
 import Model from "../assets/Model.js";
 
-
-extend({ UnrealBloomPass })
-
 function Scene() {
-
+    let light = [-9.8,2.3,-11.5];
+    const [target] = useState(() => new THREE.Object3D())
     return (
         <div className="canvas-container">
-            <Canvas >
-                <color attach="background" args={['rgba(26, 0, 68, 1.0)']} />
-                <pointLight position={[0,4,4]}/>
-                <hemisphereLight intensity={0.3} position={[5, 10, 50]} />
-                <OrbitControls />
+            <Canvas aspect={1} >
+                <PerspectiveCamera makeDefault fov={50} near={0.1} far={100}/>
+                <color attach="background" args={['black']} />
+                <spotLight
+                    position={light}
+                    castShadow
+                    target={target}
+                    penumbra={1.0}
+                    radiusTop={0.4}
+                    radiusBottom={20}
+                    distance={80}
+                    angle={0.25}
+                    attenuation={20}
+                    anglePower={5}
+                    intensity={3}
+                    opacity={0.2}
+                />
+                <primitive object={target} position={[-9.6,-4,-12]} />
+                <hemisphereLight intensity={0.6} position={[0,5,-10]} />
                 <Suspense fallback={null}>
-                    <Model position={[0,-3.6,1.4]} rotation-y={Math.PI * -.5} rotation-x={Math.PI * .02}/>
+                    <Model position={[0,-4.0,-12]} rotation-y={Math.PI * -.5} rotation-x={Math.PI * 0.03}/>
                 </Suspense>    
-                <Effects disableGamma>  
-                    <unrealBloomPass threshold={1} strength={0.5} radius={0.5}/>
-                </Effects>
                 <Stats />
-
+                <BakeShadows />
+                <Preload all />
             </Canvas>
             
         </div>
